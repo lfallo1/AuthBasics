977e11b30310b8f2b803ea590658640b2e53d1da
lfallo1 <fallon.lance@gmail.com>
2020-02-05 17:08:42 -0500

    initial commit

---
+++ AuthenticationBasics/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace AuthenticationBasics.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetHome()
+        {
+            return Ok(new { response = "Data anyone can see" });
+        }
+
+        [Authorize]
+        [HttpGet("secret")]
+        public IActionResult GetSecretInfo()
+        {
+            return Ok(new { response = "Private data" });
+        }
+
+        [HttpGet("authenticate")]
+        public IActionResult Authenticate()
+        {
+            var userClaim = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "lfallon"),
+                new Claim(ClaimTypes.Email, "lfallon@something.org")
+            };
+
+            var licenseClaim = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "lance fallon"),
+                new Claim("LicenseNumber", "340874074502702075047")
+            };
+
+            var userIdentity = new ClaimsIdentity(userClaim, "userIdentity");
+            var licenseIdentity = new ClaimsIdentity(licenseClaim, "licenseIdentity");
+
+            var userPrincipal = new ClaimsPrincipal(new[] { userIdentity, licenseIdentity });
+
+            HttpContext.SignInAsync(userPrincipal);
+
+            //Response.Cookies.Append("Auth.Cookie", "Password");
+            return Ok();
+        }
+    }
+}
